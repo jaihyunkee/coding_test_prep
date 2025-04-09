28814649ed69767238bdd5766629bd1411a64abc
Jay kee <46767671+jaihyunkee@users.noreply.github.com>
2025-04-09 19:34:10 +0900

    [level 2] Title: 더 맵게, Time: 1675.97 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,25 +1,14 @@
 import heapq
 
 def solution(scoville, K):
-    # 힙 초기화
+    answer = 0
     heapq.heapify(scoville)
-    ans = 0
-    
-    # 가장 작은 원소가 K 이상이 될 때까지 섞기
-    while len(scoville) > 1:
-        # 가장 작은 두 원소 꺼내기
-        a = heapq.heappop(scoville)
-        if a >= K:
-            return ans
-        b = heapq.heappop(scoville)
-        
-        # 새로운 스코빌 지수 생성
-        new_sc = a + (b * 2)
-        heapq.heappush(scoville, new_sc)
-        ans += 1
-
-    # 남은 하나의 스코빌 지수가 K 이상인지 체크
-    if scoville[0] >= K:
-        return ans
-    else:
+    if len(scoville) < 2 and scoville[0] < K:
         return -1
+    while scoville[0] < K and len(scoville) >= 2:
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, first + second * 2)
+        answer += 1
+    
+    return answer if scoville[0] >= K else -1
